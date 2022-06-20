cfee3c69de51e076768ac375e23ce8d82f26ab20
cif95 <vivianacifarelli9@gmail.com>
2022-06-20 22:25:42 +0200

    enhanced food show + index

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,19 @@
 const mix = require('laravel-mix');
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
 
- mix.sass('resources/sass/app.scss', 'public/css')
- .options({ processCssUrls: false }),
+mix.sass('resources/sass/app.scss', 'public/css')
+.copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
+.options({ processCssUrls: false })
 mix.js('resources/js/front.js', 'public/js'),
 mix.js('resources/js/admin.js', 'public/js'),
 mix.js('resources/js/food.js', 'public/js')
